df1a5eeca1351d9ca55a6fe034ac3461c85fb4d8
Martin Hochel <hochelmartin@gmail.com>
2017-10-18 00:54:19 +0200

    feat(test): add snapshot testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import React from 'react'
+import { render } from 'react-dom'
+import renderer from 'react-test-renderer'
+
 import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  render(<App />, div)
+})
+
+it('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import React from 'react'
+import { render } from 'react-dom'
+
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+import App from './App'
+
+import registerServiceWorker from './registerServiceWorker'
+
+render(<App />, document.getElementById('root') as HTMLElement)
 registerServiceWorker()
