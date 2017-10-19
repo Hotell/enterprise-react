db1831e70e315b322a0d3277edae7b9f6f9980c1
Martin Hochel <hochelmartin@gmail.com>
2017-10-19 09:46:21 +0200

    feat(app): add hot module reload

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,7 +5,17 @@ import './index.css'
 
 import App from './App'
 
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
-render(<App />, document.getElementById('root') as HTMLElement)
-registerServiceWorker()
+const mountNode = document.getElementById('root') as HTMLElement
+
+render(<App />, mountNode)
+// registerServiceWorker()
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    // tslint:disable-next-line:no-console
+    console.log('🔁  HMR Reloading `./App`...')
+    render(<App />, mountNode)
+  })
+}
