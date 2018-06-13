41313cb25c6a3ada1b932d297942e3dd6a683545
Martin Hochel <hochelmartin@gmail.com>
2018-06-13 14:08:01 +0200

    chore: bump dependencies and configs and add lint,format,style npm-scripts (#1)

--- my-app/src/globals.d.ts
+++ my-app/src/globals.d.ts
@@ -2,3 +2,11 @@ declare module '*.svg' {
   const img: string
   export default img
 }
+declare module '*.png' {
+  const img: string
+  export default img
+}
+declare module '*.jpg' {
+  const img: string
+  export default img
+}
